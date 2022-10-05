2a7f7fdeffc71ac7de084bc12dd6e1b955d52f66
Oderay <oderay.olivertsantoys@iesfm.onmicrosoft.com>
2022-10-05 11:48:54 +0200

    second commit

--- src/main/java/org/example/ReceivePackageSemaphore.java
+++ src/main/java/org/example/ReceivePackageSemaphore.java
@@ -7,13 +7,26 @@ public class ReceivePackageSemaphore implements Runnable{
     private Semaphore receivePackageSemaphore;
     private Semaphore sendPackageSemaphore;
 
-    public ReceivePackageSemaphore(Semaphore receivePackageSemaphore, Semaphore sendPackageSemaphore) {
+    private int packageNum;
+
+    public ReceivePackageSemaphore(Semaphore receivePackageSemaphore, Semaphore sendPackageSemaphore, int packageNum) {
         this.receivePackageSemaphore = receivePackageSemaphore;
         this.sendPackageSemaphore = sendPackageSemaphore;
+        this.packageNum = packageNum;
     }
 
     @Override
     public void run() {
 
+        for (int i = 0; i < 100; i++ ){
+
+            try {
+                receivePackageSemaphore.acquire();
+                System.out.println("Recibido paquete" + packageNum);
+                sendPackageSemaphore.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/main/java/org/example/SendPackageSemaphore.java
+++ src/main/java/org/example/SendPackageSemaphore.java
@@ -1,4 +1,34 @@
 package org.example;
 
-public class SendPackageSemaphore {
+import java.util.concurrent.Semaphore;
+
+public class SendPackageSemaphore implements Runnable {
+
+    private Semaphore sendPackageSemaphore;
+    private Semaphore receivePackageSemaphore;
+
+    private int packageNum;
+
+    public SendPackageSemaphore(Semaphore sendPackageSemaphore, Semaphore receivePackageSemaphore, int packageNum) {
+        this.sendPackageSemaphore = sendPackageSemaphore;
+        this.receivePackageSemaphore = receivePackageSemaphore;
+        this.packageNum = packageNum;
+    }
+
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 100; i++){
+            try {
+                sendPackageSemaphore.acquire();
+                System.out.println("Enviando paquete" + packageNum);
+                receivePackageSemaphore.release();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
 }
